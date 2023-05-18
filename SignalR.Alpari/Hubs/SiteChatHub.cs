44f0c9558af2a8d55fd6cc645d27891f8dec65da
Erfan Mohebi <mohammadreza.moebi@edag.com>
2023-05-18 14:26:08 +0200

    Add project files.

---
+++ SignalR.Alpari/Hubs/SiteChatHub.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Threading.Tasks;
+
+namespace SignalR.Alpari.Hubs
+{
+    public class SiteChatHub:Hub
+    {
+        public async Task SendNewMessage(string Sender,string Message)
+        {
+           await Clients.All.SendAsync("getNewMessage", Sender,Message,DateTime.Now.ToShortDateString());
+        }
+
+        public override Task OnConnectedAsync()
+        {
+            var s = Context.ConnectionId;
+            return base.OnConnectedAsync();
+        }
+
+        //When client disconnected
+        public override Task OnDisconnectedAsync(Exception exception)
+        {
+            return base.OnDisconnectedAsync(exception); 
+        }
+
+    }
+}
---
+++ SignalR.Alpari/wwwroot/js/SignalRService.js
@@ -0,0 +1,33 @@
+﻿var chatBox=$('#ChatBox');
+
+var connection = new signalR.HubConnectionBuilder()
+    .withUrl("/chathub")
+    .build();
+
+connection.start();
+
+//connection.invoke('SendNewMessage', "بازدید کننده", "این پیام از سمت کلاینت ارسال شده است");
+
+function showChatDialog() {
+    chatBox.css('dispaly', 'block');
+}
+
+function Init() {
+    setTimeout(showChatDialog(), 1000);
+
+    var NewMessageForm = $('#NewMessageForm');
+    NewMessageForm.on('submit', function (e) {
+        e.preventDefault();
+        var message = e.target[0].value;
+        e.target[0].value = '';
+        sendMessage(message);
+    });
+}
+function sendMessage(text) {
+    connection.invoke('SendNewMessage', 'بازدید کننده', text);
+}
+
+
+$(document).ready(function () { 
+    Init();
+});
